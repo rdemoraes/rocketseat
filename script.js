0c9cb0fdeb75b4f183c9a70c3dbc25b52f8c248e
Raphael Francisco de Moraes <raphael.f.moraes@gmail.com>
2024-01-16 21:38:21 -0300

    update links and images

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ function toggleMode() {
   // Substituir a imagem
   if (html.classList.contains("light")) {
     // Se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-raphael-light.png")
     img.setAttribute(
       "alt",
       (alt =
-        "Foto de Mayk Brito sorrindo, usando oculos e camisa preta, barba e fundo amarelo.")
+        "Foto de Raphael Moraes sorrindo, usando oculos e camisa branca, barba e fundo amarelo.")
     )
   } else {
     //Se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-raphael.png")
     img.setAttribute(
       "alt",
       (alt =
-        "Foto de Mayk Brito sorrindo, usando oculos e camisa preta, barba e fundo branco.")
+        "Foto de Raphael Moraes sorrindo, usando oculos e camisa branca, barba e fundo degrade.")
     )
   }
 }
